ce1f146f80b8df76c723cd90c2f259466670747f
RCode2 <rohan.sivva@gmail.com>
2024-07-19 18:10:48 -0400

    Initial Commit

---
+++ Diabuddy4/Diabuddy4App.swift
@@ -0,0 +1,17 @@
+//
+//  Diabuddy4App.swift
+//  Diabuddy4
+//
+//  Created by Srikanth Sivva on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Diabuddy4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
