45d853de74cd6ab2cd6fb8f0d51fc176a53bae03
xrobertothompson <robrider.thompson@gmail.com>
2024-08-30 17:35:40 -0400

    diabuddy 1

---
+++ Diabuddy4/File.swift
@@ -0,0 +1,54 @@
+//
+//  MenuView.swift
+//  diabuddy
+//
+//  Created by Roberto on 8/11/24.
+//
+
+//CHANGE THIS LATER TO WHAT WE HAVE IN FIGMA
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Image(systemName: "person")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Profile")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 100)
+            HStack {
+                Image(systemName: "envelope")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Messages")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            HStack {
+                Image(systemName: "gear")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Settings")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            Spacer()
+        }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(Color(red: 32/255, green: 32/255, blue: 32/255))
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+           }
+}
+
+#Preview {
+    MenuView()
+}
+
